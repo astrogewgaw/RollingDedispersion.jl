e2b45e42691e0fe54814478658e4acf5fc06f4c7
Ujjwal Panda <ujjwalpanda97@gmail.com>
2024-01-08 16:00:08 +0530

    ✨ Add a first implementation.

--- src/RollingDedispersion.jl
+++ src/RollingDedispersion.jl
@@ -1,5 +1,20 @@
 module RollingDedispersion
 
-# Write your package code here.
+const 𝓓 = 4.1488064239e3
+
+function rolldd!(I′::AbstractMatrix, I::AbstractMatrix; fₕ, Δf, δt, dm)
+    nf = size(I, 1)
+    @inbounds Threads.@threads for i ∈ axes(I, 1)
+        f = fₕ - ((i - 1) * Δf / nf)
+        ξ = round(Int, 𝓓 * dm * (f^-2 - fₕ^-2) / δt)
+        I′[i, :] .= circshift(I[i, :], -ξ)
+    end
+end
+
+function rolldd(I::AbstractMatrix; fₕ, Δf, δt, dm)
+    I′ = similar(I)
+    rolldd!(I′, I; fₕ, Δf, δt, dm)
+    I′
+end
 
 end
